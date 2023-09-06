58fc6ad0a7205f99e9ccde3951e9a39783a8243f
Mol <375753904@qq.com>
2023-09-06 16:45:18 +0900

    //initial

---
+++ leetcode/array/binary search/BinarySearch.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class BinarySearch
+    {
+        //假设数组是有序的
+        //元素不重复
+        public void main()
+        {
+
+        }
+    }
+}
